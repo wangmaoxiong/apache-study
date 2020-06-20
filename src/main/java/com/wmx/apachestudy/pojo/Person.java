c144f6ae94d4fc8d7641b12293cd8d5242c05876
wangmaoxiong <2268461750@qq.com>
2020-06-20 20:29:00 +0800

    实体对象

---
+++ src/main/java/com/wmx/apachestudy/pojo/Person.java
@@ -0,0 +1,57 @@
+package com.wmx.apachestudy.pojo;
+
+import java.util.Date;
+
+/**
+ * @author wangMaoXiong
+ * @version 1.0
+ * @date 2020/6/20 10:54
+ */
+public class Person {
+    private Integer id;
+    private String name;
+    private Date birthday;
+    private Float salry;
+
+    public Person() {
+    }
+
+    public Person(Integer id, String name, Date birthday, Float salry) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.salry = salry;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Float getSalry() {
+        return salry;
+    }
+
+    public void setSalry(Float salry) {
+        this.salry = salry;
+    }
+}
