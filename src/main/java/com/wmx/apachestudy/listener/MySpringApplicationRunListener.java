bc7f0a2dc1093317db6f3b3ec0202dc1ebb4403b
wangmaoxiong <2268461750@qq.com>
2020-06-26 17:57:57 +0800

    实现 SpringApplicationRunListener 监听器接口，监听 Spring Boot 的启动流程。

--- src/main/java/com/wmx/apachestudy/listener/MySpringApplicationRunListener.java
+++ src/main/java/com/wmx/apachestudy/listener/MySpringApplicationRunListener.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 实现 SpringApplicationRunListener 监听器接口，监听 Spring Boot 的启动流程。
@@ -15,9 +16,10 @@ import org.springframework.core.env.ConfigurableEnvironment;
 public class MySpringApplicationRunListener implements SpringApplicationRunListener {
 
     /**
-     * 必须提供构造器，否则启动失败抛出异常.
+     * 必须提供带 SpringApplication application, String[] args 参数的构造器，否则启动失败抛出异常.
+     *
      * @param application
-     * @param args
+     * @param args ：应用启动类上 main 方法传入的参数
      */
     public MySpringApplicationRunListener(SpringApplication application, String[] args) {
         System.out.println(" MySpringApplicationRunListener 构造器执行...");
@@ -46,6 +48,10 @@ public class MySpringApplicationRunListener implements SpringApplicationRunListe
     @Override
     public void started(ConfigurableApplicationContext context) {
         System.out.println(" started 方法执行...");
+        //从 Spring 容器中获取指定的 bean，然后执行操作.
+        RestTemplate restTemplate = context.getBeanFactory().getBean(RestTemplate.class);
+        String forObject = restTemplate.getForObject("https://api.apiopen.top/getJoke?page=1&count=2&type=video", String.class);
+        System.out.printf("%s%n", forObject);
     }
 
     @Override
