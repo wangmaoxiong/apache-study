b3090c7b5188a663a813b18244e3829be0815cf7
wangmaoxiong <2268461750@qq.com>
2020-06-26 17:58:17 +0800

    提供一个 RestTemplate 实例交由 Spring 容器管理

--- src/main/java/com/wmx/apachestudy/ApacheStudyApplication.java
+++ src/main/java/com/wmx/apachestudy/ApacheStudyApplication.java
@@ -2,6 +2,8 @@ package com.wmx.apachestudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author wangMaoXiong
@@ -13,4 +15,13 @@ public class ApacheStudyApplication {
         SpringApplication.run(ApacheStudyApplication.class, args);
     }
 
+    /**
+     * 提供一个 RestTemplate 实例交由 Spring 容器管理
+     *
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
