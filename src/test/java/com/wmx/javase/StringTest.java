280b245d8844b2eb3ca2275a9d2d8043930170fd
wangmaoxiong <2268461750@qq.com>
2020-12-26 14:29:10 +0800

    java.lang.String API 测试类

--- src/test/java/com/wmx/javase/StringTest.java
+++ src/test/java/com/wmx/javase/StringTest.java
@@ -3,6 +3,7 @@ package com.wmx.javase;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * java.lang.String API 测试类
@@ -12,6 +13,23 @@ import java.util.Arrays;
  * @date 2020/12/24 19:04
  */
 public class StringTest {
+
+    /**
+     * String 类型基本知识
+     */
+    @Test
+    public void test1() {
+        String s = "Hello";//jvm看到 "Hello ",在string池创建对象存储它，并将其引用返回给 s
+        //jvm看到 "Java"，在string池创建新的string对象存储它，再把新建的string对象的引用返回给s。
+        //而原先的 "Hello "仍然在string池内没有消失，等待垃圾回收，Hello 是不能被修改的。
+        s = "Java";
+
+        //因为常量池中已经有 Hello 对象，所以此时 s1 引用会直接指向它
+        String s1 = "Hello";
+        //首先 jvm 会为 xyz 字符串在 String 常量池中创建一个对象
+        //然后 new String 又会在堆内存中创建对象（非常量池），所以一共是两个对象
+        String x = new String("xyz");
+    }
 
     /**
      * replace(CharSequence target, CharSequence replacement)
@@ -67,5 +85,20 @@ public class StringTest {
         String s = "万里'长城'！长又长";
         String[] replace = s.split("['！]", 2);
         System.out.println(Arrays.asList(replace));//[万里, 长城'！长又长]
+        System.out.println(Long.MAX_VALUE);
+        System.out.println("9223372036854775807".length());
+        System.out.println(new Date().getTime());
     }
+
+    /**
+     * static String format(String format, Object... args)	使用指定的格式字符串和参数返回格式化的字符串。
+     */
+    @Test
+    public void format() {
+        // "%.2f", %：表示小数点前任意位数、2：表示两位小数、f：格式后的结果表示浮点型
+        // 此时参数必须是浮点型、如果是其它类型则报错
+        System.out.println(String.format("%.2f", 34.55666F));//34.56
+        System.out.println(String.format("%.2f", 34.5));//34.50
+    }
+
 }
